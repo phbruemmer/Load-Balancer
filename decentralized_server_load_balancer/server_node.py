2799467a12e208d78853127106f90effb1df54f6
phbruemmer <phbruemmer@gmail.com>
2025-03-15 19:12:37 +0100

    - removed async tasks for now to reduce complexity at the beginning

--- decentralized_server_load_balancer/server_node.py
+++ decentralized_server_load_balancer/server_node.py
@@ -9,6 +9,11 @@ class NodeData:
         self.port = port
         self.node_id = node_id
 
+        self.max_node_connections = 16
+
+        self.connections = 0
+        self.connected_clients = []
+
         self.task_manager = self.TaskManager
 
     class TaskManager:
@@ -16,11 +21,7 @@ class NodeData:
             self.node_data = node_data
 
             self.threads = 0
-            self.async_tasks = 0
-
-            self.max_async_tasks = 4
             self.max_threads = 4
-
             self.thread_pool = []
 
         def create_new_thread(self, target, *args) -> None:
@@ -28,6 +29,8 @@ class NodeData:
                 new_thread = threading.Thread(target=target, args=args)
                 new_thread.start()
                 self.threads += 1
+            else:
+                print("[create_new_thread] reached max thread limit.")
 
         def thread_wrapper(self, initial_target, *args) -> None:
             initial_target(args)
@@ -38,10 +41,10 @@ class NodeData:
             self.thread_pool = [thread for thread in self.thread_pool if thread.is_alive()]
             print("[update_thread_pool] updated thread pool.")
 
-        def create_new_async(self):
-            if self.async_tasks < self.max_async_tasks:
-                # create new async-task
-                pass
+    def run(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((self.ip, self.port))
+            # sock.listen(self.max_node_connections)
 
 
 if __name__ == '__main__':
